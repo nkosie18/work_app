f6ddb993c0c84e5f01b5043ec8817b0db0009b12
nkosie <nkosi.nuz@gmail.com>
2021-12-17 12:50:53 +0200

    new stuff added

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,12 @@ from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from config import Config
+from flask_heroku import Heroku
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
+heroku = Heroku(app)
 migrate =Migrate(app, db)
 
 
--- config.py
+++ config.py
@@ -1,11 +1,14 @@
 import os
+import psycopg2
 from datetime import timedelta
 
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace(
+        'postgres://', 'postgresql://') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.environ.get('SECRETE_KEY') or 'ThiS-iS-a-VeRY-seCRET-kEY'
     LDAP_LOGIN_VIEW = 'login.login'
