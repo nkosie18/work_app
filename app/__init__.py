cdea07a7ebfb3b1c19c49341194d9e5190319178
nkosie <nkosi.nuz@gmail.com>
2022-01-17 08:58:48 +0200

    fixed app again

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,6 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
-from flask_marshmallow import Marshmallow
 
 
 app = Flask(__name__)
@@ -11,7 +10,6 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
-ma = Marshmallow(app)
 
 
 
